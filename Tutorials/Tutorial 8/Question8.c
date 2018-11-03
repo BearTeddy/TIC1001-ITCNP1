7b7b93a15f245eda30abd772a287395a3bb8c109
tun myat <HtunHtetMyat@r-203-97-25-172.comp.nus.edu.sg>
2018-11-03 12:33:46 +0800

    Tutorial 8 Added

--- Tutorials/Tutorial 8/Question8.c
+++ Tutorials/Tutorial 8/Question8.c
@@ -4,25 +4,24 @@
 #include <ctype.h>
 
 void interleave(FILE *in1, FILE *in2, FILE *out) {
-    char a[1000];
-    char b[1000];
-    while (fgets(a,1000,in1) != NULL && fgets(b,1000,in2) != NULL) {
-        fputs(a,out);
-        fputs(b,out);
-    }
-    while (fgets(a,1000,in1) != NULL){
-         fputs(a,out);
-    }
-    while(fgets(b,1000,in2) != NULL){
-         fputs(b,out);
-    }
+    char *a;
+    char *b;
+    char t[1000];
+    do{
+        if(( a= fgets(t,1000,in1))){
+            fputs(t,out);
+        }
+        if(( b= fgets(t,1000,in2))){
+            fputs(t,out);
+        }
+    }while(a || b);
 }
 
 int main (void){
     FILE *fp = fopen("wind.txt","r");
-    FILE *fq =fopen("pop.text","r");
+    FILE *fq =fopen("pop.txt","r");
     FILE *f2 = fopen("copytext.txt","w+");
-    if(fp != NULL && fq != NULL && f2!=NULL){
+    if(fp != NULL && fq != NULL){
         interleave(fp,fq,f2);
         fclose(fp);
         fclose(fq);
